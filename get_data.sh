37617328405a84185d4e5104ce211c42451f886c
raoxiang <wangyiraoxiang@163.com>
2016-06-04 18:17:55 +0800

    com

--- get_data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mysqldump -h qilin99rdb1.mysql.rds.aliyuncs.com -uweb_user -P 3306 -pQilinMysql2015 -t ctrade > insert_data.sql
--- get_table.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mysqldump -h qilin99rdb1.mysql.rds.aliyuncs.com -uweb_user -P 3306 -pQilinMysql2015 -d ctrade > table_create.sql
