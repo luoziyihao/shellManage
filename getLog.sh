5471a7a1aeac8c994c61b2106cf49d70bb0709fe
luoziyihao <luoziyihao@gmail.com>
2016-08-24 14:33:46 +0800

    com

---
+++ getLog.sh
@@ -0,0 +1,16 @@
+#########################################################################
+# File Name: getLog.sh
+# Author: raoxiang
+# mail: xiangrao@qilin99.com
+# Created Time: 2016-08-10
+#########################################################################
+#!/bin/bash
+
+YESTERDAY=$(date -d "1 days ago" +%Y-%m-%d)
+TODAY=$(date -d today "+%Y-%m-%d")
+
+ssh root@cms.qilin99.com "/bin/bash /root/getTodayCatalina.sh"
+
+sleep 5
+
+scp "root@cms.qilin99.com:/tmp/"$(date -d today "+%Y-%m-%d")".catalina.out" ./
