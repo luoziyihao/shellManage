fa16a1c8955a0564d9841dca73d53493503c5c2c
luoziyihao <luoziyihao@gmail.com>
2016-12-15 19:12:30 +0800

    com

---
+++ kills.sh
@@ -0,0 +1,12 @@
+
+#########################################################################
+# File Name: kills.sh
+# Author: raoxiang
+# mail: xiangrao@qilin99.com
+# Created Time: 2016-12-15
+#########################################################################
+#!/bin/bash
+# "" 表示 str  '' 会强制转义
+PROCESS_NAME="${1}"
+echo ${PROCESS_NAME}
+ps -elf |grep -i ${PROCESS_NAME}|awk '{print($4)}'|xargs -i sudo kill -9 {}
