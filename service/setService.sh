3218f577a360f9dfe22466337a4424e8cb50ef52
luoziyihao <wangyiraoxiang@163.com>
2017-01-02 17:00:11 +0800

    modfiy AYTools

--- service/setService.sh
+++ service/setService.sh
@@ -8,7 +8,7 @@
 
 deploy_cmd=$1
 app_name=$2
-run_user=${333}
+run_user=$3
 sudo ln -fs  ${deploy_cmd} /etc/init.d/${app_name}
 sudo chmod 500 /etc/init.d/${app_name}
 sudo chown ${run_user} /etc/init.d/${app_name}
