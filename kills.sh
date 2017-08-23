dd99dc2d7bf4f7df0e9a049a84854dbc65761242
luoziyihao <wangyiraoxiang@163.com>
2017-08-23 15:32:56 +0800

    force push

--- kills.sh
+++ kills.sh
@@ -1,12 +1,23 @@
-
+#!/bin/bash
 #########################################################################
 # File Name: kills.sh
 # Author: raoxiang
 # mail: xiangrao@qilin99.com
 # Created Time: 2016-12-15
 #########################################################################
-#!/bin/bash
 # "" 表示 str, 里面可以写命令  '' 会强制转义
 PROCESS_NAME="${1}"
-echo "kill ${PROCESS_NAME}"
-sudo ps -elf | grep -i ${PROCESS_NAME}|grep -v "grep"|awk '{print($4)}'|xargs -i sudo kill -9 {}
+echo "start kill ${PROCESS_NAME}"
+
+# get ps_info
+ps_info=`sudo ps -elf |grep -v "ks" |grep -v "kills.sh"| grep -i ${PROCESS_NAME}|grep -v "grep"`
+echo "ps_info=${ps_info}"
+
+# kill
+echo ${ps_info} | awk '{print($4)}'|xargs -i sudo kill -9 {}
+
+# get ps_info
+ps_info=`sudo ps -elf |grep -v "ks" |grep -v "kills.sh"| grep -i ${PROCESS_NAME}|grep -v "grep"`
+echo "new ps_info=${ps_info}"
+
+echo "kill done "
