aa87fff478cb83fd235bc84e41b6c91a79259bb9
luoziyihao <wangyiraoxiang@163.com>
2017-08-23 15:04:29 +0800

    force push

--- kills.sh
+++ kills.sh
@@ -8,5 +8,5 @@
 #!/bin/bash
 # "" 表示 str, 里面可以写命令  '' 会强制转义
 PROCESS_NAME="${1}"
-echo ${PROCESS_NAME}
-sudo ps -elf |grep -v 'ps' | grep -i ${PROCESS_NAME}|awk '{print($4)}'|xargs -i sudo kill -9 {}
+echo "kill ${PROCESS_NAME}"
+sudo ps -elf | grep -i ${PROCESS_NAME}|grep -v "grep"|awk '{print($4)}'|xargs -i sudo kill -9 {}
