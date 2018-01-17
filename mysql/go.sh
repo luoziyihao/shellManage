6900decd3ae6a6569638dcec4661de548a4b3a14
luoziyihao <wangyiraoxiang@163.com>
2018-01-17 11:53:30 +0800

    force push

--- mysql/go.sh
+++ mysql/go.sh
@@ -3,4 +3,4 @@
 # File Name: go.sh
 # Author: raoxiang
 
-alias ownsql="mysql -h host -P 3306 -uuser -ppass --default-character-set=utf8"
+alias ownsql="mysql -h host -P 3306 -uuser -ppass --default-character-set=utf8 -D dbname"
