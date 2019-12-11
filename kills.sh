67b74709a904ba51b5ccbe402e7612bb80c97ba8
xiang.rao <wangyiraoxiang@163.com>
2019-12-11 10:29:04 +0800

    fix

--- kills.sh
+++ kills.sh
@@ -11,7 +11,7 @@ echo "start kill ${PROCESS_NAME}"
 
 find_ps_info(){
     # get ps_info
-    ps_info=`ps -elf |grep -v "ks" |grep -v "kills.sh"| grep -i ${PROCESS_NAME}|grep -v "grep"`
+    ps_info=`ps -ef |grep -v "ks" |grep -v "kills.sh"| grep -i ${PROCESS_NAME}|grep -v "grep"`
     echo "ps_info=${ps_info}"
 }
 
